2fa7d1c63a9cf7c7de7a503c47e68c389bc4e2aa
Stone Zhang <stnzhl@gmail.com>
2017-09-23 18:25:48 +0900

    [initial commit]

---
+++ STNScrollBarControl/STNScrollBarControl.h
@@ -0,0 +1,19 @@
+//
+//  STNScrollBarControl.h
+//  STNScrollBarControl
+//
+//  Created by Stone Zhang on 9/23/17.
+//  Copyright © 2017 Stone Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STNScrollBarControl.
+FOUNDATION_EXPORT double STNScrollBarControlVersionNumber;
+
+//! Project version string for STNScrollBarControl.
+FOUNDATION_EXPORT const unsigned char STNScrollBarControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STNScrollBarControl/PublicHeader.h>
+
+
